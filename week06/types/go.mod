8c9b18e3e53412bfe43d183ca284d40d31f67e1b
inhadeepblue <deepblue@inha.ac.kr>
2025-09-29 13:56:20 +0900

    v0.1 types/main.go

---
+++ week06/types/go.mod
@@ -0,0 +1,3 @@
+module types
+
+go 1.25.1
---
+++ week06/types/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Ceil(2.31))
+	fmt.Println(strings.Title("go developer~"))
+}
