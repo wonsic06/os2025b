7e4cdbaa31ef95676c1f2de684c9ef8b53df63f2
inhadeepblue <deepblue@inha.ac.kr>
2025-09-29 14:14:19 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,7 +6,10 @@ import (
 	"strings"
 )
 
+// Ctrl+F5
 func main() {
 	fmt.Println(math.Ceil(2.31))
 	fmt.Println(strings.Title("go developer~"))
+	fmt.Println("Kim\nInha\t\"\\") // C like
+	fmt.Println('A', '가')
 }
