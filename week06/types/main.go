696d75ad01fc1c10ce2c5eb4a93f7561f5878fa6
inhadeepblue <deepblue@inha.ac.kr>
2025-09-29 15:33:36 +0900

    .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,16 +6,9 @@ import (
 )
 
 func main() {
-	// Zero values
-	var f64 float64
-	var i16 int16
-	var t bool
-	var s string
-	var i int
+	// var 64f float64
+	// fmt.Println(64f, reflect.TypeOf(64f))
 
-	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(i16, reflect.TypeOf(i16))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
+	totalPrice := 1000
+	fmt.Println(totalPrice, reflect.TypeOf(totalPrice))
 }
