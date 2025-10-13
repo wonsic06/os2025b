c6256bace35146e0921c26de894c9121e1b20e09
inhadeepblue <deepblue@inha.ac.kr>
2025-10-13 14:21:09 +0900

    loops/main.go v0.1 time & strings package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strings"
+	"time"
 )
 
 func main() {
-	var length float64 = 3.2
-	var width int = 2
-	fmt.Println("면적은", int(length)*width)
-	fmt.Println("length > width?", int(length) > width)
-	fmt.Println("형변환", reflect.TypeOf(int(length)))
-	fmt.Println("원본", reflect.TypeOf(length))
+	var now time.Time = time.Now()
+	var month time.Month = now.Month() // month := now.Month()
+	var day int = now.Day()
+	fmt.Println(month, day)
+
+	univ := "Go$ Inha$"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
