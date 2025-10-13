7a10ea0de32d944e07d8e1cbf9aead062d8eae4f
inhadeepblue <deepblue@inha.ac.kr>
2025-10-13 15:23:42 +0900

    loops/main.go v0.1 strconv package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -5,14 +5,35 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	// shadowing
+	// var int int = 99
+	// var b int = 8
+	// var fmt string = "inha"
+	// fmt.Println(int, b)
+
+	fmt.Print("Enter a score: ")
 	r := bufio.NewReader(os.Stdin)
 	i, err := r.ReadString('\n')
-	//fmt.Println(err)
 	if err != nil {
-		log.Fatal(err) // report the error, exit the program
+		log.Fatal(err)
 	}
-	fmt.Println(i)
+
+	i = strings.TrimSpace(i)                // 문자열 주위에 붙은 공란 및 탭 키 등 제거
+	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if score >= 60 {
+		status = "Pass"
+	} else {
+		status = "Fail"
+	}
+	fmt.Println(score, status)
 }
